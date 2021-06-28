551d4e0ff129cb943b6ad05148e24fa0cca84213
Ansh <anshtandonat@gmail.com>
2021-06-28 22:55:52 +0530

    added 3 more fragments and item selected listener

--- app/src/main/java/com/example/galleryapp/fragments/SlideShowFragment.java
+++ app/src/main/java/com/example/galleryapp/fragments/SlideShowFragment.java
@@ -1,26 +1,30 @@
 package com.example.galleryapp.fragments;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
-
 import com.example.galleryapp.R;
 
+public class SlideShowFragment extends Fragment {
 
-public class RecentFragment extends Fragment {
 
+    public SlideShowFragment() {
+        // Required empty public constructor
+    }
 
-    public RecentFragment ()
-    {
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recent, container, false);
+        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
